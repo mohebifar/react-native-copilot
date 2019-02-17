2854e5b6477e9d7ac15a5a483a6430a9364e64a8
Lou Klepner <lklepner@gmail.com>
2019-02-17 08:50:39 -0500

    crash fix - check for this.wrapper before this.wrapper.measure

--- src/components/ConnectedCopilotStep.js
+++ src/components/ConnectedCopilotStep.js
@@ -65,7 +65,7 @@ class ConnectedCopilotStep extends Component<Props> {
     return new Promise((resolve, reject) => {
       const measure = () => {
         // Wait until the wrapper element appears
-        if (this.wrapper.measure) {
+        if (this.wrapper && this.wrapper.measure) {
           this.wrapper.measure(
             (ox, oy, width, height, x, y) => resolve({
               x, y, width, height,
