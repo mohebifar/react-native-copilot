71f6b4046b898312bcca81f271fb2088decf4778
Xavier Carpentier <xcapetir@gmail.com>
2020-04-27 22:36:41 +0200

    Update AnimatedPath.js

--- src/components/AnimatedPath.js
+++ src/components/AnimatedPath.js
@@ -1,25 +1,7 @@
-import React, { Component } from 'react';
-
 import { Animated } from 'react-native';
 import { Path } from 'react-native-svg';
 
-class SvgPathWrap extends Component {
-  setNativeProps = (props) => {
-    if (this._component) {
-      this._component.setNativeProps(props);
-    }
-  }
 
-  render() {
-    return (
-      <Path
-        ref={(component) => { this._component = component; }}
-        {...this.props}
-      />
-    );
-  }
-}
-
-const AnimatedSvgPath = Animated.createAnimatedComponent(SvgPathWrap);
+const AnimatedSvgPath = Animated.createAnimatedComponent(Path);
 
 export default AnimatedSvgPath;
