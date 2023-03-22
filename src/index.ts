99269d08a51eac2af883c39f5a9c0c4e2f06a807
Mohamad Mohebifar <mohamad@mohebifar.com>
2023-03-22 10:52:10 -0400

    Export DefaultUI components

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import { StepNumber } from "./components/default-ui/StepNumber";
+import { Tooltip } from "./components/default-ui/Tooltip";
 export { walkthroughable } from "./hocs/walkthroughable";
 export { CopilotStep } from "./components/CopilotStep";
 export { CopilotProvider, useCopilot } from "./contexts/CopilotProvider";
@@ -6,3 +8,8 @@ export type {
   StepNumberProps,
   TooltipProps,
 } from "./types";
+
+export const DefaultUI = {
+  StepNumber,
+  Tooltip,
+};
